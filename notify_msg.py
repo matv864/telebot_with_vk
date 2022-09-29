a79c33f1ef119680542d2ea9dd00d565dd30afb5
matv864 <91239322+matv864@users.noreply.github.com>
2022-09-29 12:56:11 +1000

    Update notify_msg.py

--- notify_msg.py
+++ notify_msg.py
@@ -4,5 +4,7 @@ def notify(receive, username, id_, text):
     msg_vk = f"пользователь под ником {username} \n"
     msg_vk += f"с id - {id_} \n"
     msg_vk += "отправил вам сообщение \n"
-    msg_vk += f'"{text}"'
-    vk_notify(msg_vk, receive)
+    msg_vk += f'"{text}"\n'
+    msg_vk += "это сообщение отправил вам этот бот\n"
+    msg_vk += "https://t.me/matv864_bot\n"
+    return vk_notify(msg_vk, receive)
