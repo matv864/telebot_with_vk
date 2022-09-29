2ee24cba7d2e548938ee8863379ce1b2afeeb8cc
matv864 <91239322+matv864@users.noreply.github.com>
2022-09-29 12:56:46 +1000

    Update vk_file.py

--- vk_file.py
+++ vk_file.py
@@ -8,9 +8,12 @@ import random
 
 
 def vk_notify(msg, receive):
-    req = {"user_id": receive, 
-           "random_id": round(random.random() * 100),
-           "message": msg}
-    session = vk_api.VkApi(token=vk_token)
-    something = session.method("messages.send", req)
-    return something
+       req = {"user_id": receive, 
+              "random_id": round(random.random() * 100),
+              "message": msg}
+       session = vk_api.VkApi(token=vk_token)
+       try:
+              something = session.method("messages.send", req)
+              return something
+       except:
+              return 0
